107c97aa030e8348c65850369457f81beb93c4b2
huhreenee <hariniuiyer29@gmail.com>
2024-04-05 23:36:44 +0530

    added input validations and handles NANs and divide by 0

--- index.js
+++ index.js
@@ -4,39 +4,78 @@ document
     let credits = [];
     let sgpas = [];
     for (let i = 1; i <= 4; i++) {
-      credits.push(
-        parseFloat(document.getElementById(`sem${i}_credits`).value) 
+      credits.push(parseInt(document.getElementById(`sem${i}_credits`).value));
+      sgpas.push(
+        parseFloat(document.getElementById(`sem${i}_sgpa`).value) || 0
       );
-      sgpas.push(parseFloat(document.getElementById(`sem${i}_sgpa`).value) || 0);
     }
 
     let final_cgpa = 0;
     let total_credits = 0;
     for (let i in credits) {
       final_cgpa += credits[i] * sgpas[i];
-      if (sgpas[i] == 0){
-        total_credits += 0;
-      }
-      else {
+      if (sgpas[i] != 0) {
         total_credits += credits[i];
       }
     }
     final_cgpa /= total_credits;
-    console.log(final_cgpa);
+    // console.log(final_cgpa);
+    if (total_credits == 0) {
+      document.getElementById("result").innerText = "Enter Valid SGPAs";
+      return;
+    }
     document.getElementById("result").innerText = final_cgpa;
   });
 
-let def_credits = [26,24,14,14]
-document
-  .getElementById("Reset")
-  .addEventListener("click", function (event) {
-    // console.log(event)
-    for (let i = 0; i < 4; i++){
-      document.getElementById(`sem${i+1}_credits`).value = def_credits[i]
-    }
-    // let myinput = document.getElementById("sem4_credits");
-    // myinput.value = 0
-  });
+let def_credits = [26, 24, 14, 14];
+document.getElementById("Reset").addEventListener("click", function (event) {
+  // console.log(event.target.id)
+  for (let i = 0; i < 4; i++) {
+    document.getElementById(`sem${i + 1}_credits`).value = def_credits[i];
+  }
+  // let myinput = document.getElementById("sem4_credits");
+  // myinput.value = 0
+});
 
-  // var myinput = document.getElementById("myinput").value.defaultValue;
-  // myinput.value = myinput.defaultValue;
+// var myinput = document.getElementById("myinput").value.defaultValue;
+// myinput.value = myinput.defaultValue;
+function notNumber(event) {
+  // console.log(event)
+  if (event.target.id.includes("credits")) {
+    if (
+      !(
+        (event.keyCode >= 48 && event.keyCode <= 57) ||
+        event.keyCode == 8 ||
+        event.key == "ArrowLeft" ||
+        event.key == "ArrowRight"
+      )
+    ) {
+      event.preventDefault();
+      return false;
+    }
+  }
+  else {
+    if (
+      !(
+        (event.keyCode >= 48 && event.keyCode <= 57) ||
+        event.keyCode == 8 ||
+        event.keyCode == 190 ||
+        event.key == "ArrowLeft" ||
+        event.key == "ArrowRight"
+      )
+    ) {
+      event.preventDefault();
+      return false;
+    }
+  }
+  // return true;
+}
+
+for (let i = 1; i <= 4; i++) {
+  document
+    .getElementById(`sem${i}_credits`)
+    .addEventListener("keydown", notNumber);
+  document
+    .getElementById(`sem${i}_sgpa`)
+    .addEventListener("keydown", notNumber);
+}
