2fdc5467c466c2aee03589a03e2f4e9acb7604f1
huhreenee <hariniuiyer29@gmail.com>
2024-04-05 22:31:58 +0530

    handled default values and added reset

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 document
   .getElementById("Calculate")
-  .addEventListener("click", function(event) {
+  .addEventListener("click", function (event) {
     let credits = [];
     let sgpas = [];
     for (let i = 1; i <= 4; i++) {
       credits.push(
-        parseFloat(document.getElementById(`sem${i}_credits`).value)
+        parseFloat(document.getElementById(`sem${i}_credits`).value) 
       );
-      sgpas.push(parseFloat(document.getElementById(`sem${i}_sgpa`).value));
+      sgpas.push(parseFloat(document.getElementById(`sem${i}_sgpa`).value) || 0);
     }
 
     let final_cgpa = 0;
     let total_credits = 0;
     for (let i in credits) {
       final_cgpa += credits[i] * sgpas[i];
-      total_credits += credits[i];
+      if (sgpas[i] == 0){
+        total_credits += 0;
+      }
+      else {
+        total_credits += credits[i];
+      }
     }
     final_cgpa /= total_credits;
     console.log(final_cgpa);
-    document.getElementById("result").innerText = final_cgpa
+    document.getElementById("result").innerText = final_cgpa;
   });
+
+let def_credits = [26,24,14,14]
+document
+  .getElementById("Reset")
+  .addEventListener("click", function (event) {
+    // console.log(event)
+    for (let i = 0; i < 4; i++){
+      document.getElementById(`sem${i+1}_credits`).value = def_credits[i]
+    }
+    // let myinput = document.getElementById("sem4_credits");
+    // myinput.value = 0
+  });
+
+  // var myinput = document.getElementById("myinput").value.defaultValue;
+  // myinput.value = myinput.defaultValue;
